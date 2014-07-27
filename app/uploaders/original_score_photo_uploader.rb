d6d6d7d362d2699d1ea630422325f43c0e12a5c1
Colin Trent <trentcs@gmail.com>
2014-07-27 15:56:51 -0500

    txt

--- app/uploaders/original_score_photo_uploader.rb
+++ app/uploaders/original_score_photo_uploader.rb
@@ -39,7 +39,7 @@ class OriginalScorePhotoUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list  
-    %w(jpg jpeg gif png xml pdf)
+    %w(jpg jpeg gif png xml pdf txt)
   end
 
   # Override the filename of the uploaded files:
