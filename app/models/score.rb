4e6458e53a5c49e9b08262174ab18f9f4b8270a3
Spencer Olson <spence1818@gmail.com>
2014-07-27 23:34:05 -0500

    setting up db seeding

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,7 +1,21 @@
+require "rexml/document"
+
 class Score < ActiveRecord::Base
   belongs_to :user
   has_many :parts
   has_many :measures, through: :parts
   has_many :notes, through: :measures
   mount_uploader :original_image, OriginalScorePhotoUploader
+
+  before_create :create_parts
+
+  def create_parts
+    music_xml_file = open(self.music_xml)
+    $doc = REXML::Document.new music_xml_file
+
+    score_parts = $doc.elements.to_a("//score-part")
+    score_parts.each do |score_part, index|
+      self.parts << Part.create(instrument_name: score_part.elements.to_a("//instrument-name")[0].text)
+    end
+  end
 end
