e8152551bb58ee09ca41ef46a8dd3430aaa85a97
Colin Trent <trentcs@gmail.com>
2014-07-30 19:25:37 -0500

    chart buttons

--- app/assets/javascripts/anazlyze-tabs.js
+++ app/assets/javascripts/anazlyze-tabs.js
@@ -1,13 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function(){
   $('#pitch_range').show();
-  $('li').click(function(){
-    var hrefValue = $(this).children().attr("href")
+  $(window).resize();
+  $("li").click(function(){
+    var hrefValue = $(this).children().attr("href");
     $('.active').removeClass("active");
-    $(this).addClass("active");
+    $(this).children().eq(0).addClass("active");
+  
     $(".chart").hide();
     console.log(hrefValue);
     $(hrefValue).show();
-  })
+   $(window).resize();
+  });
+});
 
 
-})
+
