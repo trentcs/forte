0ce370a9d3c69c8f3b12680521af85f7c619abd9
Andy Principe <andyprincipe@gmail.com>
2014-07-30 20:16:53 -0500

    more model testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,12 @@
-require 'coveralls'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
-# require 'database_cleaner'
-require 'shoulda/matchers'
-require 'coveralls'
-Coveralls.wear!
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'database_cleaner'
+require 'shoulda/matchers'
+require 'coveralls'
+Coveralls.wear!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
